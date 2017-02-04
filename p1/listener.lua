ddc3e12d487ff70e453950c6d8978b8e4ec9a2a7
root <root@localhost.localdomain>
2017-02-04 16:57:35 -0500

    add automation to calculate average for each channel

--- p1/listener.lua
+++ p1/listener.lua
@@ -1,4 +1,4 @@
--- This program will register a menu that will open a window with a count of occurrences
+--- This program will register a menu that will open a window with a count of occurrences
 -- of every address in the capture
 value = Field.new('radiotap.dbm_antsignal')
 --frame_type = Field.new('wlan.fc.type_subtype')
@@ -6,7 +6,7 @@ frame_num=0
 total = 0
 local function menuable_tap()
         -- Declare the window we will use
-        local tw = TextWindow.new("Address Counter")        
+        --local tw = TextWindow.new("Address Counter")        
         -- This will contain a hash of counters of appearances of a certain address
         local ips = {}
 
@@ -19,7 +19,7 @@ local function menuable_tap()
         end
 
         -- we tell the window to call the remove() function when closed
-        tw:set_atclose(remove)
+        --tw:set_atclose(remove)
 
         -- this function will be called once for each packet
         function tap.packet(pinfo,tvb,tapdata)                  
@@ -29,10 +29,14 @@ local function menuable_tap()
                     frame_num = frame_num + 1
                     avg = total / frame_num
   --              bea = frame_type()
-                    print(tostring(avg)..' '..tostring(frame_num))
+                   -- print(tostring(avg)..' '..tostring(frame_num))
                 end
                 local src = ips[tostring(pinfo.src)] or 0
                 local dst = ips[tostring(pinfo.dst)] or 0
+		-- when reach maximum packets number, print out average
+		if (frame_num == 20) then
+			print(tostring(avg))
+		end
 
                 ips[tostring(pinfo.src)] = src + 1
                 ips[tostring(pinfo.dst)] = dst + 1
@@ -40,20 +44,21 @@ local function menuable_tap()
 
         -- this function will be called once every few seconds to update our window
         function tap.draw(t)
-                tw:clear()
+                --tw:clear()
                 for ip,num in pairs(ips) do
-                        tw:append(ip .. "\t" .. num .. "\n");
+                        --tw:append(ip .. "\t" .. num .. "\n");
                 end
         end
 
         -- this function will be called whenever a reset is needed
         -- e.g. when reloading the capture file
         function tap.reset()
-                tw:clear()
+                --tw:clear()
                 ips = {}
         end
 end
 
 -- using this function we register our function
 -- to be called when the user selects the Tools->Test->Packets menu
-register_menu("Test/Packets", menuable_tap, MENU_TOOLS_UNSORTED)
+--register_menu("Test/Packets", menuable_tap, MENU_TOOLS_UNSORTED)
+menuable_tap()
